f8bcd0b6449b9f14c311574779d15a588c5c50c5
nobunobuta <nobunobuta@nobunobuta.com>
2007-12-10 06:02:47 +0000

    SimpleLink サンプル修正

--- xoops_trust_path/libs/NBFrame/examples/xoops_root_path/modules/simplelink/class/SimpleLinkDefaultAction.class.php
+++ xoops_trust_path/libs/NBFrame/examples/xoops_root_path/modules/simplelink/class/SimpleLinkDefaultAction.class.php
@@ -2,7 +2,31 @@
 NBFrame::using('Action');
 
 class SimpleLinkDefaultAction extends NBFrameAction {
+    var $mList;
+    function prepare() {
+        parent::prepare();
+        $this->setDefaultTemplate($this->prefix('main.html'));
+    }
+
+    function executeDefaultOp() {
+        $linkHandler =& NBFrame::getHandler('SimpleLinkLink', $this->mEnvironment);
+        $categoryHandler =& NBFrame::getHandler('SimpleLinkCategory', $this->mEnvironment);
+        $criteria =& new CriteriaElement();
+        $criteria->setSort('category_weight');
+        $categoryObjects =& $categoryHandler->getNestedObjects($criteria, '');
+        $this->mList = array();
+        foreach($categoryObjects as $categoryObject) {
+            $criteria =& new Criteria('link_category_id', $categoryObject->getVar('category_id'));
+            $criteria->setSort('link_weight');
+            $linkObjects = $linkHandler->getObjects($criteria);
+            $categoryObject->assignVar('link_objects', $linkObjects);
+            $this->mList[] = $categoryObject;
+        }
+        return NBFRAME_ACTION_VIEW_DEFAULT;
+    }
+    
     function viewDefaultOp() {
-        echo "Test Print for Main Action";
+        $this->mXoopsTpl->assign('mydirname', $this->mDirName);
+        $this->mXoopsTpl->assign('simplelink_list', $this->mList);
     }
 }
