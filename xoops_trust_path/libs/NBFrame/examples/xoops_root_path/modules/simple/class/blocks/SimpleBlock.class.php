eb141d3c40de1bffa4c18c8d8a58295de57d0703
nobunobuta <nobunobuta@nobunobuta.com>
2007-12-20 08:53:24 +0000

    SVNメンテナンス

---
+++ xoops_trust_path/libs/NBFrame/examples/xoops_root_path/modules/simple/class/blocks/SimpleBlock.class.php
@@ -0,0 +1,12 @@
+<?php
+if (!class_exists('SimpleBlock')) {
+    class SimpleBlock {
+        function edit($environment, $option) {
+        }
+        
+        function show($environment, $option){
+            $block['content'] = 'Hello World';
+            return $block;
+        }
+    }
+}
