ddaf57e0ae897b2455a6781e01db0da0a9a5e7df
nobunobuta <nobunobuta@nobunobuta.com>
2008-01-08 14:43:21 +0000

    my_languageでのオーバーライド定義に対応

--- xoops_trust_path/libs/NBFrame/class/NBFrameLanguage.class.php
+++ xoops_trust_path/libs/NBFrame/class/NBFrameLanguage.class.php
@@ -8,16 +8,22 @@ if (!class_exists('NBFrameLanguage')) {
         var $mEnvironment;
         var $mSalt ;
         var $mCachePath ;
+        var $mMyLangPath;
 
         function NBFrameLanguage($target, $inAdmin=false) {
             $this->mEnvironment =& NBFrame::getEnvironments($target);
             $this->mDirName = $this->mEnvironment->mDirName;
             $this->mSalt = substr( md5( XOOPS_ROOT_PATH . XOOPS_DB_USER . XOOPS_DB_PREFIX ) , 0 , 6 );
             if (defined('XOOPS_TRUST_PATH') && file_exists(XOOPS_TRUST_PATH.'/cache')) {
-        	    $this->mCachePath = XOOPS_TRUST_PATH.'/cache' ;
-        	} else {
-        	    $this->mCachePath = XOOPS_ROOT_PATH.'/cache' ;
-        	}
+                $this->mCachePath = XOOPS_TRUST_PATH.'/cache' ;
+            } else {
+                $this->mCachePath = XOOPS_ROOT_PATH.'/cache' ;
+            }
+            if( defined( 'ALTSYS_MYLANGUAGE_ROOT_PATH' ) && file_exists( ALTSYS_MYLANGUAGE_ROOT_PATH ) ) {
+                $this->mMyLangPath = ALTSYS_MYLANGUAGE_ROOT_PATH ;
+            } else {
+                $this->mMyLangPath = XOOPS_ROOT_PATH.'/my_language' ;
+            }
             if (!empty($this->mEnvironment->mOrigDirName)) {
                 $this->mOrigDirName = $this->mEnvironment->mOrigDirName;
             } else {
@@ -38,7 +44,7 @@ if (!class_exists('NBFrameLanguage')) {
             }
             $this->setInAdmin($inAdmin);
             if (defined('NBFRAME_BASE_DIR') && file_exists(NBFRAME_BASE_DIR.'/language/'.$GLOBALS['xoopsConfig']['language'].'/NBFrameCommon.php')) {
-                $this->_load(NBFRAME_BASE_DIR.'/language/'.$GLOBALS['xoopsConfig']['language'].'/NBFrameCommon.php');
+                require NBFRAME_BASE_DIR.'/language/'.$GLOBALS['xoopsConfig']['language'].'/NBFrameCommon.php';
             }
         }
 
@@ -52,35 +58,32 @@ if (!class_exists('NBFrameLanguage')) {
 
         function loadModuleLanguageFile($filename) {
             $languageFile=null;
-        	$cacheFile = $this->_getCacheFileName( $filename , $this->mDirName) ;
-        	
-        	if (file_exists($cacheFile)) {
-        	    $languageFile = $cacheFile;
-        	    require_once $languageFile;
-        	} else {
+            $cacheFile = $this->_getCacheFileName( $filename , $this->mDirName) ;
+            $myLangFile = $this->mMyLangPath.'/modules/'.$this->mDirName.'/'.$GLOBALS['xoopsConfig']['language'].'/'.$filename;
+            if (file_exists($myLangFile)) {
+                require_once $myLangFile ;
+                $originalErrorLevel = error_reporting() ;
+                error_reporting( $originalErrorLevel & ~ E_NOTICE ) ;
+            }
+            
+            if (file_exists($cacheFile)) {
+                $languageFile = $cacheFile;
+                require_once $languageFile;
+            } else {
                 $fileOffset = '/modules/'.$this->mOrigDirName.'/language/'.$GLOBALS['xoopsConfig']['language'].'/'.$filename;
                 if (defined('XOOPS_TRUST_PATH') && file_exists(XOOPS_TRUST_PATH. $fileOffset)) {
                     $languageFile = XOOPS_TRUST_PATH.$fileOffset;
-            	    require $languageFile;
+                    require $languageFile;
                 } else if (file_exists(XOOPS_ROOT_PATH.'/common'.$fileOffset)) {
                     $languageFile =  XOOPS_ROOT_PATH.'/common'.$fileOffset;
-            	    require $languageFile;
+                    require $languageFile;
                 }
             }
             if (!empty($languageFile)) {
-//                $this->_load($languageFile);
-	            if (!defined('_MYLANGADMIN_'.md5($languageFile))) define('_MYLANGADMIN_'.md5($languageFile) ,1);
+                if (!defined('_MYLANGADMIN_'.md5($languageFile))) define('_MYLANGADMIN_'.md5($languageFile) ,1);
             }
-        }
-
-        function _load($languageFile) {
-            if (!empty($languageFile)) {
-//                if (defined('XOOPS_CUBE_LEGACY')) {
-//                    $root=&XCube_Root::getSingleton();
-//                    $root->mLanguageManager->_loadFile($languageFile);
-//                } else {
-                    require $languageFile;
-//                }
+            if (file_exists($myLangFile)) {
+                error_reporting( $originalErrorLevel) ;
             }
         }
 
