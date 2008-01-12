a5485e40c07eab86df0d06cac2de4de17836a33d
nobunobuta <nobunobuta@nobunobuta.com>
2008-01-12 12:13:47 +0000

    Fix TYPO

--- xoops_trust_path/libs/NBFrame/examples/html/modules/simple/preload/NBPreload.class.php
+++ xoops_trust_path/libs/NBFrame/examples/html/modules/simple/preload/NBPreload.class.php
@@ -1,5 +1,5 @@
 <?php
 include dirname(dirname(__FILE__)).'/include/NBFrameLoader.inc.php';
-$preloadEnvironment =& NBFrame::prepare('', NBFRAME_TARGET_BLOCK);
+$preloadEnvironment =& NBFrame::getEnvironments(NBFRAME_TARGET_TEMP);
 @include NBFRAME_BASE_DIR.'/include/NBFramePreload.inc.php';
 ?>
