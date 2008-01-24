6221c833822d82319518384f7ad755e45ccf47a9
nobunobuta <nobunobuta@nobunobuta.com>
2008-01-24 08:09:09 +0000

    FIX TYPO

--- xoops_trust_path/libs/NBFrame/class/NBFrameObjectAction.class.php
+++ xoops_trust_path/libs/NBFrame/class/NBFrameObjectAction.class.php
@@ -105,6 +105,7 @@ if (!class_exists('NBFrameObjectAction')) {
         }
 
         function setObjectKeyField() {
+            $object = $this->mObjectHandler->create();
             $objectKey = $this->mObjectHandler->getKeyFields();
             $this->mObjectKeyField = $objectKey[0];
         }
