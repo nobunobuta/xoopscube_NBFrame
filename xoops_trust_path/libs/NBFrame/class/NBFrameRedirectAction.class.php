25d688292e00cde6058aea404c440a327e364fd9
nobunobuta <nobunobuta@nobunobuta.com>
2008-02-07 02:31:37 +0000

    FireFoxでのredirect_header後の管理画面ページ遷移の改善

---
+++ xoops_trust_path/libs/NBFrame/class/NBFrameRedirectAction.class.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @package NBFrame
+ * @version $Id$
+ * @copyright Copyright 2007 NobuNobuXOOPS Project <http://sourceforge.net/projects/nobunobuxoops/>
+ * @author NobuNobu <nobunobu@nobunobu.com>
+ * @license http://www.gnu.org/licenses/gpl.txt GNU GENERAL PUBLIC LICENSE Version 2
+ *
+ */
+if (!class_exists('NBFrameRedirectAction')) {
+    NBFrame::using('Action');
+    class NBFrameRedirectAction extends NBFrameAction {
+        function executeDefaultOp() {
+            if (isset($_REQUEST['NBFrameNextAction'])) {
+                $action=basename($_REQUEST['NBFrameNextAction']);
+            } else {
+                $action='';
+            }
+            if (isset($_GET['NBFrameNextOp'])) {
+                $op=basename($_GET['NBFrameNextOp']);
+            } else {
+                $op='';
+            }
+            $paramArray = array();
+            foreach($_GET as $key=>$param) {
+                if(($key!='action') && !preg_match('/^NBFrame/', $key)) {
+                    $paramArray[$key]=$param;
+                }
+            }
+            header('Location: '.NBFrame::getActionUrl($this->mEnvironment, $action, $paramArray, '',false, false));
+            exit();
+        }
+    }
+}
